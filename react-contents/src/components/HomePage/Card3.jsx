06c3e5975402faa7b77624ba7bed2c265b4e9202
samiksha275 <agarwalsamiksha88@gmail.com>
2025-06-08 03:30:51 +0530

    Added the cards to the homepage

---
+++ react-contents/src/components/HomePage/Card3.jsx
@@ -0,0 +1,9 @@
+export default function Card3() {
+  return (
+    <div className="text-center w-60 h-60 bg-blue-500 px-10 flex flex-col justify-center items-center my-10 gap-2 rounded-3xl">
+      <div className="text-5xl text-white font-bold">1000+</div>
+      <div className="text-2xl text-white">Donations</div>
+      <div className="font-serif text-white text-lg">Support that creates real impact</div>
+    </div>
+  );
+}
--- react-contents/src/pages/HomePage.jsx
+++ react-contents/src/pages/HomePage.jsx
@@ -1,9 +1,17 @@
 import WelcomeMessage from "../components/HomePage/WelcomeMessage";
+import Card1 from "../components/HomePage/Card1";
+import Card2 from "../components/HomePage/Card2";
+import Card3 from "../components/HomePage/Card3";
 
 export default function HomePage() {
   return (
     <div>
         <WelcomeMessage/>
+        <div className="flex justify-center items-center gap-10">
+            <Card1/>
+            <Card2/>
+            <Card3/>
+        </div>
     </div>
   );
 }
