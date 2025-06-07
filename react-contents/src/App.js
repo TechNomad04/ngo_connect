fade2649a53c8a83976d57697f7071cd113a1097
samiksha275 <agarwalsamiksha88@gmail.com>
2025-06-08 03:01:53 +0530

    Added the welcome message for the home page

--- react-contents/src/App.js
+++ react-contents/src/App.js
@@ -1,9 +1,14 @@
-import Navbar from "./components/Navbar";
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
 
 function App(){
   return (
     <div>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ react-contents/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+import WelcomeMessage from "../components/HomePage/WelcomeMessage";
+
+export default function HomePage() {
+  return (
+    <div>
+        <WelcomeMessage/>
+    </div>
+  );
+}
