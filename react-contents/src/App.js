9e10c1dc5922e2b95d980d0741718c919b088fa6
samiksha275 <agarwalsamiksha88@gmail.com>
2025-06-08 23:07:23 +0530

    Added the login page design

--- react-contents/src/App.js
+++ react-contents/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Navbar from './components/Navbar';
+import LoginPage from './pages/LoginPage';
 
 function App(){
   return (
@@ -8,6 +9,7 @@ function App(){
       <Navbar/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
       </Routes>
     </div>
   );
