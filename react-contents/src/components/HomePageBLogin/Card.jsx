f8095fb0dc7dc50cd0e9c73689facb3852218e82
samiksha275 <agarwalsamiksha88@gmail.com>
2025-06-14 23:42:23 +0530

    Deleted everything

--- react-contents/src/components/HomePageBLogin/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card({ stats, category, description }){
-    return (
-        <div className="text-center w-60 h-60 bg-blue-500 px-10 flex flex-col justify-center items-center my-10 gap-2 rounded-3xl">
-            <div className="text-5xl text-white font-bold">{stats}</div>
-            <div className="text-2xl text-white">{category}</div>
-            <div className="font-serif text-white text-lg">{description}</div>
-        </div>
-    );
-}
